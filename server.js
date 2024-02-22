612dca87b6d32608a315773a8bd27b7d55001804
maddy023 <madeshselvarani@gmail.com>
2024-02-22 12:43:46 +0530

    fix: review poiints

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send("Hello World! yasdoy my man!"));
+app.get("/", (req, res) => res.send("Hello World! yasdoasdasy my man!"));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
